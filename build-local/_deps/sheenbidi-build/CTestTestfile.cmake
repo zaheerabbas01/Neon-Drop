82bfd38e88be7fe9b59905c11438cb8dc3da3583
zaheerabbas01 <zaheerszhrs@gmail.com>
2026-08-30 16:43:42 +0500

    Ignore generated build and editor files

--- build-local/_deps/sheenbidi-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zaheer-abbas/Games/youtube/game 1/build-local/_deps/sheenbidi-src
-# Build directory: /home/zaheer-abbas/Games/youtube/game 1/build-local/_deps/sheenbidi-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
